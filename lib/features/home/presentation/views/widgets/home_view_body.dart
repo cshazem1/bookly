3ec42fceb1a05e67c3f28cb166750bfc583bd534
AQ <cshazemelwazery@gmail.com>
2024-04-07 17:18:50 +0200

    add featured  list view to home

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,7 +10,11 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar(), CustomListViewItem()],
+      children: [
+        CustomAppBar(),
+        FeatureBooksListView(),
+      ],
     );
   }
 }
+

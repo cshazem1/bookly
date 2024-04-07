93399f56ca8435022ee6736de5b4de8956116b16
AQ <cshazemelwazery@gmail.com>
2024-04-07 15:51:17 +0200

    add custom app bar

---
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(AssetsData.logo, height: 20),
+          IconButton(
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass),
+            onPressed: () {},
+            iconSize: 24,
+          )
+        ],
+      ),
+    );
+  }
+}

ac70f106a4f3532b315f26e7df463b34cf154a99
AQ <cshazemelwazery@gmail.com>
2024-04-07 03:23:36 +0200

    adding sliding animation to slash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,21 +1,50 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
-class SplashViewBody extends StatelessWidget{
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this, duration: 1.seconds);
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+@override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo).animate().fade(delay: 1000.ms).fade(),
-        const SizedBox(height: 5,),
-        const Text('Read Free Books',textAlign: TextAlign.center,).animate().fade(delay: 1700.ms).shake()
+      children: [
+        Image.asset(AssetsData.logo)
+            .animate()
+            .fade(delay: 500.ms, duration: 800.ms)
+            .fade(),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingText( slidingAnimation: slidingAnimation)
       ],
     );
   }
-
 }
+

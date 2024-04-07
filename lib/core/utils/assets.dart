032c6e86ee89081ad6817d0d4419034756697f9f
AQ <cshazemelwazery@gmail.com>
2024-04-07 16:36:51 +0200

    add custom list view item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,4 @@
 class AssetsData{
 static const logo='assets/images/Logo.png';
-
+static const testJungleBook='assets/images/test_jungle_book.jpeg';
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,8 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Column(
-      children: [CustomAppBar()],
+      children: [CustomAppBar(), CustomListViewItem()],
     );
   }
 }
-

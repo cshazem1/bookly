b778c766dc55c54c3622617b6a261c7ed52dd205
AQ <cshazemelwazery@gmail.com>
2024-04-16 17:27:27 +0200

    add tittle & subtitle to book item to book details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_app_bar.dart';
@@ -10,8 +9,8 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width=MediaQuery.of(context).size.width;
-    return  SafeArea(
+    var width = MediaQuery.of(context).size.width;
+    return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30.0),
         child: Column(
@@ -19,21 +18,31 @@ class BookDetailsViewBody extends StatelessWidget {
           children: [
             const CustomBookDetailsAppBar(),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: width*.17,),
+              padding: EdgeInsets.symmetric(
+                horizontal: width * .2,
+              ),
               child: const CustomBookImage(),
             ),
-            const SizedBox(height: 6,),
-            const Text(
-              "TheJunGleBook",
-              style: Styles.textStyle30,
+            const SizedBox(
+              height: 43,
             ),
-            const SizedBox(height: 6,),
-            const Text(
-              "The Legend Well",
-              style: Styles.textStyle20,
+            Text(
+              "The JunGle Book",
+              style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
             ),
-            const SizedBox(height: 6,),
-            const BookRating()
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: .7,
+              child: Text(
+                "The Legend Well",
+                style: Styles.textStyle20.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
 
           ],
         ),

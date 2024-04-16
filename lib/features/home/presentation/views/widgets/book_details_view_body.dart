074c196a2c4e27c97d637fc88d435d7157d39908
AQ <cshazemelwazery@gmail.com>
2024-04-16 23:20:33 +0200

    create details section and similar section

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,20 +1,16 @@
-import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/features/home/presentation/views/widgets/similar_books_details_list_view.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
+import 'book_details_section.dart';
 import 'books_action.dart';
 import 'custom_book_details_app_bar.dart';
-import 'custom_book_image.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-    return SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        padding: EdgeInsets.symmetric(horizontal: 30.0),
         child: CustomScrollView(
           slivers: [
             SliverFillRemaining(
@@ -22,69 +18,23 @@ class BookDetailsViewBody extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const CustomBookDetailsAppBar(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: width * .15,
-                    ),
-                    child: const CustomBookImage(),
-                  ),
-                  const SizedBox(
-                    height: 43,
-                  ),
-                  Text(
-                    "The JunGle Book",
-                    style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 6,
-                  ),
-                  Opacity(
-                    opacity: .7,
-                    child: Text(
-                      "The Legend Well",
-                      style: Styles.textStyle20.copyWith(
-                        fontWeight: FontWeight.w500,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  const BookRating(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  ),
-                  const SizedBox(
+                  CustomBookDetailsAppBar(),
+                  BookDetailsSection(),
+                  SizedBox(
                     height: 37,
                   ),
-                  const BooksAction(),
-                  const Expanded(
+                  BooksAction(),
+                  Expanded(
                     child: SizedBox(
                       height: 50,
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "The JunGle Book",
-                      style: Styles.textStyle14.copyWith(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
+                  SimilarBooksSection(),
                   SizedBox(
-                      height: MediaQuery.of(context).size.height * .15,
-                      child: const SimilarBooksDetailsListView()),
-                  const SizedBox(
                     height: 40,
                   ),
                 ],
               ),
-
             )
           ],
         ),
@@ -92,3 +42,5 @@ class BookDetailsViewBody extends StatelessWidget {
     );
   }
 }
+
+
